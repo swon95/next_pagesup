f2135dbc92f29a9bc3160e41dc03584a0e0a98b7
swon95 <shimtkddnjs13@gmail.com>
2023-09-22 10:58:11 +0900

    feat: loading effect

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Container from "@/components/Container";
 import useNotices from "@/hooks/useNotices";
+import Loader from "@/components/Loader";
 
 const List = () => {
     // const notices = [
@@ -18,8 +19,8 @@ const List = () => {
 
     const { notices, loading, error } = useNotices();
 
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error.message}</div>;
+    if (loading) return <Loader />;
+    if (error) return <p>Error: {error.message}</p>;
 
     return (
         <Container>
@@ -27,16 +28,16 @@ const List = () => {
                 <div className="flex flex-wrap justify-center relative">
                     <div className="flex flex-col items-center w-1/2 ">
                         <div className="flex flex-row justify-between w-full items-center mt-10 ">
-                            <div className="flex justify-center items-center h-16 mb-7 text-3xl font-semibold">
+                            <div className="flex justify-center items-center h-16 mb-7 text-3xl font-semibold text-[#222222]">
                                 공지사항
                             </div>
                             <input
-                                className="w-[280] h-[40] py-3 pl-3 pr-11 mb-7 border-2 border-black"
+                                style={{ width: 280, height: 40 }}
+                                className="py-3 pl-3 pr-11 mb-7 rounded-md border-2 border-black text-sm text-[#CBCBCB]"
                                 type="text"
                                 placeholder="검색어"
                             />
                         </div>
-
                         <div className="w-full border-t-2"></div>
                         <div className="flex flex-col justify-center align-middle m-auto w-full">
                             {notices.map((notice, index) => (
@@ -44,12 +45,23 @@ const List = () => {
                                     key={index}
                                     className="flex-grow p-6 hover:bg-[#EFF0F3] cursor-pointer"
                                 >
-                                    <p className="text-lg">{notice.title}</p>
-                                    <p className="text-xs">{notice.content}</p>
+                                    <p className="text-lg text-[#222222]">
+                                        {/* 글자수 제한 처리 */}
+                                        {notice.title.length > 100
+                                            ? `${notice.title.substring(
+                                                  0,
+                                                  100
+                                              )}`
+                                            : notice.title}
+                                    </p>
+                                    <p className="text-lg text-[#707070]">
+                                        {notice.content}
+                                    </p>
                                 </div>
                             ))}
                         </div>
                         <div className="w-full border-2"></div>
+                        ddd
                     </div>
                 </div>
             </div>
